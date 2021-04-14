029161d426f47bf868c8a81f3fdca89bb6740830
ahmedbechir.zouaoui <ahmedbechir.zouaoui@esprit.tn>
2021-04-15 00:40:21 +0100

    Ajoutez des fichiers projet.

---
+++ ConsomiTounsiFront/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsomiTounsiFront.MvcApplication" Language="C#" %>
---
+++ ConsomiTounsiFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConsomiTounsiFront.Startup))]
+namespace ConsomiTounsiFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
